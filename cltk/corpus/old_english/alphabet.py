681170d58de61d50bec6cca9ca5753844506c3f6
Chatziargyriou Eleftheria <36453738+Sedictious@users.noreply.github.com>
2018-12-10 19:14:17 +0200

    Added distinction between short and long vowels

--- cltk/corpus/old_english/alphabet.py
+++ cltk/corpus/old_english/alphabet.py
@@ -3,10 +3,14 @@
 # digits [1-10]
 DIGITS = ['ān', 'tƿeġen', 'þrēo', 'fēoƿer', 'fīf', 'seox', 'seofon', 'eahta', 'niġon', 'tīen']
 
-ALPHABET = ['a', 'æ', 'b', 'c', 'd', 'ð', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'ƿ', 'x', 'y', 'þ']
+ALPHABET = ['a', 'ā', 'æ', 'ǣ', 'b', 'c', 'd', 'ð', 'e', 'ē' 'f', 'g', 'h', 'i', 'ī', 'l', 'm', 'n', 'o', 'ō', 'p', 'r', 's', 't', 'u', 'ū', 'w', 'ƿ', 'x', 'y', 'ȳ', 'þ']
 
 CONSONANTS = ['b', 'c', 'cȝ', 'cg', 'd', 'ð', 'f', 'ff', 'ȝ', 'g', 'h', 'l', 'n', 'p', 'r', 's', 'ss', 'sc', 't', 'þ', 'þþ', 'ƿ', 'w']
 
-VOWELS = ['a', 'æ', 'e', 'i', 'o', 'u', 'y']
+VOWELS = ['a', 'ā', 'æ', 'ǣ', 'e', 'ē', 'i', 'ī', 'o', 'ō', 'u', 'ū', 'y', 'ȳ']
 
-DIPTHONGS = ['ea', 'eo', 'ie']
+SHORT_VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
+
+LONG_VOWELS = ['ā', 'æ', 'ǣ', 'ē', 'ī', 'ō', 'ū', 'ȳ']
+
+DIPTHONGS = ['ea', 'ēa', 'eo', 'ēo', 'ie']
